507f3c4c810180e09d914b7b96a9ee70c86c8b7c
jpbr66 <jburgos1000@gmail.com>
2022-09-21 13:50:32 -0600

    Creación del proyecto

---
+++ Segunda_Tarea_BD/Segunda_Tarea_BD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Segunda_Tarea_BD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
