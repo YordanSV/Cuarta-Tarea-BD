8fa9696209b0cf5d0100766243913d9c59fee5f6
José Pablo Burgos Retana <110577659+jpbr66@users.noreply.github.com>
2022-10-07 17:19:26 -0600

    Delete Segunda_Tarea_BD directory

--- Segunda_Tarea_BD/Segunda_Tarea_BD/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Segunda_Tarea_BD.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
