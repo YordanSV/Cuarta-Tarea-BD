e79583b732c4dd3b8a0ba9a2a01adadf549ea838
jpbr66 <jburgos1000@gmail.com>
2022-10-07 17:41:04 -0600

    Interfaz

---
+++ WebApplication1/Controllers/LoginController.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LoginController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Login
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public async Task<IActionResult> Ingresar([Bind("id,Nombre,Clave")] Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                SqlConnection con = new SqlConnection("Data Source=JPBR66\\SQLEXPRESS;" +
+                    "Initial Catalog=PrimerTarea;Integrated Security=SSPI");
+                SqlCommand cmd = new SqlCommand();
+                int result = 0;
+                cmd.Connection = con;
+                cmd.CommandText = "proc_loginUsuario";
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                con.Open();
+                cmd.Parameters.AddWithValue("@inNombre", usuario.Nombre);
+                cmd.Parameters.AddWithValue("@inClave", usuario.Clave);
+                SqlParameter retorno = cmd.Parameters.Add("@outResult", SqlDbType.Int);
+                retorno.Direction = ParameterDirection.Output;
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+                result = (int)retorno.Value;
+                if (result == 6000)
+                    ViewData["CI"] = "Combinación de usuario/password no existe en la BD";
+                if (result != 5001 && result != 6000)
+                    return RedirectToAction("Index", "Articulo");
+            }
+            return View("Index");
+        }
+
+
+
+        /*public async Task<IActionResult> Ingresar([Bind("id,Nombre,Clave")] Usuario usuario)
+        {
+            return View();/*
+            if (ModelState.IsValid)
+            {
+                _context.Add(articulo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            
+            return View(articulo);
+        }*/
+
+        // GET: Login/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Usuario == null)
+            {
+                return NotFound();
+            }
+            
+
+            var usuario = await _context.Usuario
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return View(usuario);
+        }
+
+        // GET: Login/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Login/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("id,Nombre,Clave")] Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(usuario);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(usuario);
+        }
+
+        // GET: Login/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Usuario == null)
+            {
+                return NotFound();
+            }
+
+            var usuario = await _context.Usuario.FindAsync(id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+            return View(usuario);
+        }
+
+        // POST: Login/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("id,Nombre,Clave")] Usuario usuario)
+        {
+            if (id != usuario.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(usuario);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UsuarioExists(usuario.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(usuario);
+        }
+
+        // GET: Login/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Usuario == null)
+            {
+                return NotFound();
+            }
+
+            var usuario = await _context.Usuario
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return View(usuario);
+        }
+
+        // POST: Login/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Usuario == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Usuario'  is null.");
+            }
+            var usuario = await _context.Usuario.FindAsync(id);
+            if (usuario != null)
+            {
+                _context.Usuario.Remove(usuario);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UsuarioExists(int id)
+        {
+          return _context.Usuario.Any(e => e.id == id);
+        }
+    }
+}
