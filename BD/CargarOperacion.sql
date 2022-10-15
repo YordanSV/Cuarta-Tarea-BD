4284c99c7db3d50748f2bc7f8dc07dc7eded2cc1
YordanSV <yordan10_pv@hotmail.com>
2022-10-15 14:42:44 -0600

    .

--- BD/CargarOperacion.sql
+++ BD/CargarOperacion.sql
@@ -1,6 +1,8 @@
 DECLARE @Fechas TABLE (fechaOperacion DATE);  
 
 DECLARE @Persona TABLE(nombre VARCHAR(128), valorDocId BIGINT, idTipoDoc VARCHAR(128), email VARCHAR(128), telefono1 BIGINT, telefono2 BIGINT);
+DECLARE @Propiedad TABLE(NumeroFinca INT,MetrosCuadrados INT,tipoUsoPropiedad VARCHAR(128),tipoZonaPropiedad VARCHAR(128),NumeroMedidor INT,ValorFiscal INT)
+
 
 ----Declaracion de variables----
 DECLARE @fechaItera DATE, @fechaFin DATE, @lo INT,@hi INT; -- una DECLARE siempre termina en ;
@@ -13,7 +15,7 @@ SET @xmlOperacion = (SELECT *FROM OPENROWSET(BULK 'C:\Users\Usuario\Downloads\Op
 DECLARE @hdoc int;  
 EXEC sp_xml_preparedocument @hdoc OUTPUT, @xmlOperacion
 INSERT @Fechas(fechaOperacion)
-SELECT Fecha = CONVERT(DATE,Fecha,103)
+SELECT Fecha = CONVERT(DATE,Fecha,102)
 FROM OPENXML(@hdoc,'/Datos/Operacion',1) --Cargamos todas las fechas en la tabla @Fechas
 WITH(
     Fecha VARCHAR(12)
@@ -27,9 +29,9 @@ BEGIN
     if not exists (select 1 from @Fechas where fechaOperacion=@fechaItera)
             continue
 
-    SET @fechaNodo = CONVERT(VARCHAR(20),@fechaItera,105) --Necesitamos un aux para leer @fechaItera como tipo VARCHAR
+    SET @fechaNodo = CONVERT(VARCHAR(20),@fechaItera,23) --Necesitamos un aux para leer @fechaItera como tipo VARCHAR
 		
-    ----Nuevo Empleado-----
+    ----Nueva Persona-----
     INSERT @Persona(nombre , valorDocId, idTipoDoc , email, telefono1, telefono2)
     SELECT T.Item.value('@Nombre','VARCHAR(128)'),
         T.Item.value('@ValorDocumentoIdentidad','BIGINT'),
@@ -40,6 +42,7 @@ BEGIN
     FROM @xmlOperacion.nodes('/Datos/Operacion[@Fecha=sql:variable("@FechaNodo")]/Personas/Persona') as T(Item)
 	BEGIN TRANSACTION insertarPersona
 
+	--INSERT dbo.Persona(Nombre,ValorDocId,IdTipoDoc,email,telefono1,telefono2)
 	SELECT 
 		p.nombre,
 		p.valorDocId,
@@ -48,13 +51,41 @@ BEGIN
 		p.telefono1,
 		p.telefono2
 	FROM @Persona p
-	INNER JOIN TipoDocIdent d
-	ON p.idTipoDoc = d.Nombre;
+	INNER JOIN dbo.TipoDocIdent d
+	ON p.idTipoDoc = d.Nombre
 
 	COMMIT TRANSACTION insertarPersona
 	--SELECT * FROM @Persona
+	
+	-----------Insertar Propiedades-------------
+	
+    INSERT @Propiedad TABLE(NumeroFinca ,MetrosCuadrados ,tipoUsoPropiedad ,tipoZonaPropiedad ,NumeroMedidor ,ValorFiscal )
+
+    SELECT T.Item.value('@NumeroFinca','INT'),
+        T.Item.value('@MetrosCuadrados','FLOAT'),
+        T.Item.value('@tipoUsoPropiedad','VARCHAR(128)'),
+        T.Item.value('@tipoZonaPropiedad','VARCHAR(128)'),
+        T.Item.value('@NumeroMedidor','INT'),
+        T.Item.value('@ValorFiscal','INT')
+    FROM @xmlOperacion.nodes('/Datos/Operacion[@Fecha=sql:variable("@FechaNodo")]/Propiedades/Propiedad') as T(Item)
+	BEGIN TRANSACTION insertarPropiedad
+
+	INSERT dbo.Propiedad(ID
+	SELECT 
+		p.NumeroFinca,
+
+	FROM @Propiedad p
+		INNER JOIN dbo.TipoTerreno t ON t.Nombre = p.tipoUsoPropiedad
+		INNER JOIN dbo.TipoZona z ON z.Nombre = p.tipoZonaPropiedad	
+
+	COMMIT TRANSACTION insertarPropiedad
+
+
 
 	DELETE @Persona 
     
     SET @fechaItera= (DATEADD(DAY,1,@fechaItera)) --Siguiente
+
+
+
 END;
